e74a3aeff7c4fe09e2b3657edd1e223c8350a824
massawegodii <massawegodfrey43@gmail.com>
2024-11-14 13:48:15 +0300

    header and footer for message section

--- src/components/Cat.js
+++
@@ -1,8 +0,0 @@
-import { Avatar } from '@mui/material'
-import React from 'react'
-
-export const Cat = () => {
-  return (
-    <Avatar src="https://img.freepik.com/free-psd/3d-rendering-hair-style-avatar-design_23-2151869145.jpg?t=st=1731486278~exp=1731489878~hmac=39ed24195428fb73d0a1465c1997e8271640abd444b880d469ff067701a798bf&w=740"/>
-  )
-}
--- src/pages/dashboard/GeneralApp.js
+++ src/pages/dashboard/GeneralApp.js
@@ -1,15 +1,32 @@
 import { React } from "react";
 import Chats from "./Chats";
+import { Box, Stack, useTheme } from "@mui/material";
+import Conversation from "../../components/Conversation";
 
 const GeneralApp = () => {
+  const theme = useTheme();
 
   return (
-    <>
-       {/* Chats */}
+    <Stack direction={"row"} sx={{ width: "100%" }}>
+      {/* Chats */}
 
-       <Chats />
+      <Chats />
 
-    </>
+
+      <Box
+        sx={{
+          height: "100%",
+          width: "calc(100vw - 420px)",
+          backgroundColor: theme.palette.mode === "light" ? "#F8FAFF" : theme.palette.background.paper,
+          boxShadow: "0px 0px 2px rgba(0, 0, 0, 0.25)",
+        }}
+      >
+        {/* Conversation */}
+
+        <Conversation />
+
+      </Box>
+    </Stack>
   );
 };
 
