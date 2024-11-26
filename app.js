ca82fd0f47117f4e606f7039e5044b9fb26e0a22
massawegodii <massawegodfrey43@gmail.com>
2024-11-26 23:46:21 +0300

    create environment

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require("express"); //web framwork build on top of nodeJs
+
+const morgan = require("morgan"); //HTTP request logger middleware for node.js
+
+const app = express();
+
+module.exports = app;
